ff9e5921c34436b92810c4e510216b6d0a013af1
Fresh <solovev-matveyka@inbox.ru>
2023-04-19 17:41:23 +0700

    Added MainActivity logic

--- app/src/main/java/com/fresh/registerticket/MainActivity.java
+++ app/src/main/java/com/fresh/registerticket/MainActivity.java
@@ -3,6 +3,9 @@ package com.fresh.registerticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.fresh.registerticket.ticket.Ticket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Ticket ticket = new Ticket();
+        EditText fieldID = findViewById(R.id.fieldID);
+        EditText fieldCost = findViewById(R.id.fieldCost);
+        String userID = String.format("Пользователь #%d", ticket.getUserID());
+        String cost = String.format("%d монет", ticket.getCost());
+
+        fieldID.setText(userID);
+        fieldCost.setText(cost);
     }
 }
